11ca0f2fe017adafe9197357fa31295520669e97
roy@maroonstudios.com <roy@maroonstudios.com>
2018-05-23 15:43:03 +0800

    Added the include guard

--- src/amber/constants.h
+++ src/amber/constants.h
@@ -1,2 +1,7 @@
+#ifndef AMBER_CONSTANTS_H
+#define AMBER_CONSTANTS_H
+
 extern const std::string approvedrequests_stream = "authoritynodes";
 extern const std::string authorityrequest_stream = "authorityrequests";
+
+#endif
