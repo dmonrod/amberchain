8671fed2f8db1068c9dec19a9fb61fe62c9878ee
roy@maroonstudios.com <roy@maroonstudios.com>
2018-06-01 09:59:56 +0800

    Cleanup new amber includes

--- src/amber/constants.h
+++
@@ -1,8 +0,0 @@
-#ifndef AMBER_CONSTANTS_H
-#define AMBER_CONSTANTS_H
-
-extern const std::string approvedrequests_stream = "authoritynodes";
-extern const std::string authorityrequest_stream = "authorityrequests";
-extern const std::string admin_streams[1] = {"authoritynodes"};
-
-#endif
---
+++ src/amber/utils.h
@@ -0,0 +1,11 @@
+#ifndef AMBER_CONSTANTS_H
+#define AMBER_CONSTANTS_H
+
+#include <string>
+
+#define STREAM_AUTHNODES "authoritynodes"
+#define STREAM_AUTHREQUESTS "authorityrequests"
+
+std::string SampleFunction();
+
+#endif
