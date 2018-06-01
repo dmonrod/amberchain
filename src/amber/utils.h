fa9f46761115047319cbda89a29547740e5b6d0a
oliviademetria <olivia.demetria@maroonstudios.com>
2018-06-01 10:52:30 +0800

    Add stream variables

--- src/amber/utils.h
+++ src/amber/utils.h
@@ -3,8 +3,11 @@
 
 #include <string>
 
-#define STREAM_AUTHNODES "authoritynodes"
-#define STREAM_AUTHREQUESTS "authorityrequests"
+#define STREAM_AUTHNODES            "authoritynodes"
+#define STREAM_AUTHREQUESTS         "authorityrequests"
+#define STREAM_TRANSACTIONPARAMS    "transactionparams"
+#define KEY_TRANSACTIONFEE          "min-relay-tx-fee"
+#define KEY_ADMINADDRESS            "admin-address"
 
 std::string SampleFunction();
 
